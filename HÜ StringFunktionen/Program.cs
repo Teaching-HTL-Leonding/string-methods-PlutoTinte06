ba33e4a6090a8d1662e2deff44e3706177f5ec78
PlutoTinte06 <113624288+PlutoTinte06@users.noreply.github.com>
2023-03-15 19:08:02 +0100

    Add files via upload

---
+++ HÜ StringFunktionen/Program.cs
@@ -0,0 +1,44 @@
+﻿string input = Console.ReadLine()!;
+char j = char.Parse(Console.ReadLine()!);
+int index = 0;
+
+bool Contains(string input, char j)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == j) return true;
+
+    }
+
+    return false;
+}
+
+Console.WriteLine(Contains(input, j));
+
+int IndexOf(string input, char j, int index)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == j) { index = i; break; }
+        else if (!(input.Contains(j))) {index = -1;}
+
+    }
+
+    return index;
+}
+
+Console.WriteLine(IndexOf(input, j, index));
+
+int LastIndexOf(string input, char j, int index)
+{
+     for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == j) { index = i; }
+        else if (!(input.Contains(j))) {index = -1;}
+
+    }
+
+    return index;
+}
+
+Console.WriteLine(LastIndexOf(input, j, index));
